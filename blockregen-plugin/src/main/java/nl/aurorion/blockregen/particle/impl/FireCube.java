25596fea2cf343160d31a92f1862d252f9fcf111
gustavhaavik <gkhaavik@gmail.com>
2025-01-18 22:39:40 +0100

    fixing fire cube particles

--- blockregen-plugin/src/main/java/nl/aurorion/blockregen/particle/impl/FireCube.java
+++ blockregen-plugin/src/main/java/nl/aurorion/blockregen/particle/impl/FireCube.java
@@ -6,6 +6,7 @@ import com.cryptomorin.xseries.particles.XParticle;
 import nl.aurorion.blockregen.particle.AbstractParticle;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.util.BoundingBox;
 import org.jetbrains.annotations.NotNull;
 
 public class FireCube extends AbstractParticle {
@@ -20,7 +21,11 @@ public class FireCube extends AbstractParticle {
         World world = location.getWorld();
         if (world == null) return;
 
-        ParticleDisplay display = ParticleDisplay.of(XParticle.FLAME).withLocation(location);
-        Particles.cube(location, location, 1, display);
+        final BoundingBox boundingBox = location.getBlock().getBoundingBox();
+        Location start = boundingBox.getMin().toLocation(world);
+        Location end = boundingBox.getMax().toLocation(world);
+
+        ParticleDisplay display = ParticleDisplay.of(XParticle.FLAME);
+        Particles.structuredCube(start, end, 0.2, display);
     }
 }
