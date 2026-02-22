c9b4f4a855236b195c3638b99978fce7109036fe
czmdav <18117740+czmdav@users.noreply.github.com>
2026-02-22 17:31:07 +0100

    feat: MMOItems drops (#57)
    * feat: provide MMOItem drops
    
    * Add contributors list
    
    * fix: documentation link
    
    * fix: transform ids to upper case
    
    * ref: pass whole regeneration context to item providers
    
    * add past contributors
    
    * fix: moved class
    
    * fix: NPE on preset load
    
    ---------
    
    Co-authored-by: Wertik <jregvx@gmail.com>

--- blockregen-plugin/src/main/java/nl/aurorion/blockregen/compatibility/provider/MMOItemsProvider.java
+++ blockregen-plugin/src/main/java/nl/aurorion/blockregen/compatibility/provider/MMOItemsProvider.java
@@ -4,57 +4,50 @@ import io.lumine.mythic.lib.api.item.NBTItem;
 import net.Indyuce.mmoitems.MMOItems;
 import net.Indyuce.mmoitems.api.Type;
 import net.Indyuce.mmoitems.api.block.CustomBlock;
+import net.Indyuce.mmoitems.api.item.build.ItemStackBuilder;
 import net.Indyuce.mmoitems.api.item.mmoitem.MMOItem;
+import net.Indyuce.mmoitems.api.player.PlayerData;
 import nl.aurorion.blockregen.BlockRegenPlugin;
+import nl.aurorion.blockregen.Context;
 import nl.aurorion.blockregen.ParseException;
 import nl.aurorion.blockregen.compatibility.CompatibilityProvider;
 import nl.aurorion.blockregen.compatibility.material.MMOItemsMaterial;
 import nl.aurorion.blockregen.conditional.Condition;
+import nl.aurorion.blockregen.drop.ItemProvider;
 import nl.aurorion.blockregen.material.BlockRegenMaterial;
 import nl.aurorion.blockregen.material.MaterialProvider;
+import nl.aurorion.blockregen.util.Text;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
-import org.jspecify.annotations.Nullable;
+import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.NonNull;
 
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
-public class MMOItemsProvider extends CompatibilityProvider implements MaterialProvider {
-    public MMOItemsProvider(BlockRegenPlugin plugin) {
-        super(plugin, "mmoitems");
-        setFeatures("materials", "conditions");
-    }
+public class MMOItemsProvider extends CompatibilityProvider implements ItemProvider, MaterialProvider {
 
     private static final Pattern ITEM_PATTERN = Pattern.compile("(\\S+):(\\S+)");
+
+    public MMOItemsProvider(BlockRegenPlugin plugin) {
+        super(plugin, "mmoitems");
+        setFeatures("materials", "conditions", "drops");
+    }
 
     @Override
     public void onLoad() {
         // Register conditions provider.
-        // https://gitlab.com/phoenix-dvpmt/mmoitems/-/wikis/Main%20API%20Features#checking-if-an-itemstack-is-from-mi
+        // https://docs.phoenixdevt.fr/mmoitems/api/main.html#checking-if-an-itemstack-is-from-mi
         plugin.getPresetManager().getConditions().addProvider(getPrefix() + "/tool", ((key, node) -> {
-
-            Matcher matcher = ITEM_PATTERN.matcher((String) node);
-
-            if (!matcher.matches()) {
-                throw new ParseException("Invalid input for MMOItems tool. Has to have the format of <type>:<id>.");
-            }
-
-            String typeName = matcher.group(1);
-
-            Type type = MMOItems.plugin.getTypes().get(typeName);
-
-            if (type == null) {
-                throw new ParseException("Invalid MMOItems item type " + typeName + ".");
-            }
-
-            String id = matcher.group(2);
-
-            MMOItem item = MMOItems.plugin.getMMOItem(type, id);
+            final MMOItem item = getMMOItem((String) node);
 
             if (item == null) {
-                throw new ParseException("Invalid MMOItems item '" + type + ":" + id + "'.", true);
+                throw new ParseException("Invalid MMOItems item '" + node + "'.", true);
             }
 
             return Condition.of((ctx) -> {
@@ -71,13 +64,13 @@ public class MMOItemsProvider extends CompatibilityProvider implements MaterialP
 
                 String toolType = nbtItem.getType();
 
-                if (!toolType.equalsIgnoreCase(typeName)) {
+                if (!toolType.equalsIgnoreCase(item.getType().getName())) {
                     return false;
                 }
 
                 String toolId = nbtItem.getString("MMOITEMS_ITEM_ID");
 
-                if (!toolId.equalsIgnoreCase(id)) {
+                if (!toolId.equalsIgnoreCase(item.getId())) {
                     return false;
                 }
 
@@ -122,5 +115,74 @@ public class MMOItemsProvider extends CompatibilityProvider implements MaterialP
     @Override
     public BlockRegenMaterial createInstance(java.lang.reflect.Type type) {
         return new MMOItemsMaterial(-1);
+    }
+
+    @Override
+    public @Nullable ItemStack createItem(@NonNull String id, int amount, @NonNull Context context) {
+        final Player player = context.get("player", Player.class);
+        final Block block = context.get("block", Block.class);
+
+        final MMOItem mmoItem = getMMOItem(id, player);
+
+        if (mmoItem == null) {
+            return null;
+        }
+
+        ItemStackBuilder itemBuilder = mmoItem.newBuilder();
+        itemBuilder.getLore().setLore(itemBuilder.getLore().getLore().stream()
+                .map(s -> Text.parse(s, player, block))
+                .collect(Collectors.toList()));
+
+        if (itemBuilder.getMeta().hasDisplayName()) {
+            itemBuilder.getMeta().setDisplayName(Text.parse(itemBuilder.getMeta().getDisplayName(), player, block));
+        }
+
+        ItemStack itemStack = itemBuilder.build();
+        if (itemStack != null) {
+            itemStack.setAmount(amount);
+        }
+
+        return itemStack;
+    }
+
+    @Override
+    public @Nullable ItemStack createItem(@NonNull String id, @NonNull Function<String, String> parser, int amount) {
+        // not called unless the other one is unimplemented
+        return null;
+    }
+
+    @Override
+    public boolean exists(@NotNull String id) {
+        return getMMOItem(id) != null;
+    }
+
+    private MMOItem getMMOItem(@NotNull String id) {
+        return getMMOItem(id, null);
+    }
+
+    @Nullable
+    private MMOItem getMMOItem(@NotNull String id, @Nullable Player player) {
+        Matcher matcher = ITEM_PATTERN.matcher(id);
+
+        if (!matcher.matches()) {
+            throw new ParseException("Invalid input for MMOItem. Has to have the format of '<type>:<id>'.");
+        }
+
+        String typeName = matcher.group(1).toUpperCase();
+        Type type = MMOItems.plugin.getTypes().get(typeName);
+
+        if (type == null) {
+            throw new ParseException("Invalid MMOItems item type '" + typeName + "'.");
+        }
+
+        String itemId = matcher.group(2).toUpperCase();
+
+        if (player == null) {
+            return MMOItems.plugin.getMMOItem(type, itemId);
+        }
+
+        PlayerData playerData = MMOItems.plugin.getPlayerDataManager().get(player);
+
+        return MMOItems.plugin.getMMOItem(type, itemId, playerData);
     }
 }
